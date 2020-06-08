abfbb3b34b8e346ecb0cc13f785d90eda3d8d191
Ragnar Fischer <ragnarfischer@outlookde>
2020-06-08 08:54:13 +0200

    ---

--- Object.java
+++ Object.java
@@ -1,33 +1,29 @@
-
-/**
- * Beschreiben Sie hier die Klasse Object.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Object
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Object
-     */
-    public Object()
+    private String name;
+    private String description;
+    private String content;
+    
+    public Object (String name, String description, String content)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        this.name = name;
+        this.description = description;
+        this.content = content;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
+    
+    public String getName()
     {
-        // tragen Sie hier den Code ein
-        return x + y;
+        return name;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public String interact()
+    {
+        //Zurückgeben des Contents. Am besten über Array, bei dem erstes Feld Art der Variable impliziert und zweites den Inhalt trägt // Oder separates Content-Objekt mit versch. Konsturktoren, die das dann irgendwie regeln
+        return content;
     }
 }
